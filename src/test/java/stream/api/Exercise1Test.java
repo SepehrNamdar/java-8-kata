31bbc4952ccddcc0f7c60b5a825504fc19241fc5
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2020-01-16 15:45:24 +0100

    add stream.api#Exercise2Test

--- src/test/java/stream/api/Exercise1Test.java
+++ src/test/java/stream/api/Exercise1Test.java
@@ -6,6 +6,7 @@ import common.tool.util.AssertUtil;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -32,5 +33,23 @@ public class Exercise1Test extends ClassicOnlineStore {
         List<Customer> richCustomer = richCustomerStream.collect(Collectors.toList());
         assertThat(richCustomer, hasSize(2));
         assertThat(richCustomer, contains(customerList.get(3), customerList.get(7)));
+    }
+
+    @Test
+    public void howOldAreTheCustomers() {
+        List<Customer> customerList = this.mall.getCustomerList();
+
+        /**
+         * Create a {@link Stream} from customerList with age values.
+         * Use method reference(best) or lambda expression(okay) for creating {@link Function} which will
+         * convert {@link Customer} to {@link Integer}, and then apply it by using {@link Stream#map}.
+         */
+        Function<Customer, Integer> getAgeFunction = null;
+        Stream<Integer> ageStream = null;
+
+        assertTrue(AssertUtil.isLambda(getAgeFunction));
+        List<Integer> ages = ageStream.collect(Collectors.toList());
+        assertThat(ages, hasSize(10));
+        assertThat(ages, contains(22, 27, 28, 38, 26, 22, 32, 35, 21, 36));
     }
 }
