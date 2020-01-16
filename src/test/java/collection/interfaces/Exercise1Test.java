608df92d22c0ace863266760eb15c5225ef9dc06
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2020-01-16 12:12:36 +0100

    add collection.interfaces#Exercise1Test#whoHaveNoEInYourName

--- src/test/java/collection/interfaces/Exercise1Test.java
+++ src/test/java/collection/interfaces/Exercise1Test.java
@@ -5,8 +5,11 @@ import common.tool.entity.Customer;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
@@ -27,5 +30,20 @@ public class Exercise1Test extends ClassicOnlineStore {
         // customerIterable.
 
         assertThat(nameList.toString(), is("[Joe, Steven, Patrick, Diana, Chris, Kathy, Alice, Andrew, Martin, Amy]"));
+    }
+
+    @Test
+    public void whoHaveNoEInYourName() {
+        Collection<String> nameCollection =
+                new ArrayList<>(Arrays.asList("Joe", "Steven", "Patrick", "Chris"));
+
+        /**
+         * Create a {@link Predicate} which predicates if the input string contains "e".
+         * Remove elements from {@link nameCollection} which contains "e" using {@link Collection#removeIf}.
+         */
+        Predicate<String> predicate = null;
+        // nameCollection.
+
+        assertThat(nameCollection.toString(), is("[Patrick, Chris]"));
     }
 }
