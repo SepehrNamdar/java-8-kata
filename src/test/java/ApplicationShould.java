a105d1873deb64095a3bc8338e10a61cae8fced2
SepehrNamdar <sepehr_gsfc@yahoo.com>
2021-03-13 22:50:48 +0100

    Create a testable environment

---
+++ src/test/java/ApplicationShould.java
@@ -0,0 +1,9 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationShould {
+    @Test
+    void works() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
