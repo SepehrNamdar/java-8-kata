adf579c99d35fb8e57f96bc78ee1e7090f9c3b3d
SepehrNamdar <sepehr_gsfc@yahoo.com>
2021-03-13 23:56:52 +0100

    Create Streams

--- src/test/java/ApplicationShould.java
+++
@@ -1,9 +0,0 @@
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-public class ApplicationShould {
-    @Test
-    void works() {
-        Assertions.assertThat(true).isTrue();
-    }
-}
---
+++ src/test/java/StreamCreatorShould.java
@@ -0,0 +1,70 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Random;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StreamCreatorShould {
+
+    @Test
+    void create_an_empty_stream() {
+        Stream<String> emptyStream = Stream.empty();    // Avoid null
+
+        assertThat(emptyStream).isEmpty();
+    }
+
+    @Test
+    void create_a_collection_stream() {
+        Collection<Integer> collection = Arrays.asList(1, 2, 3);    // Collection : List, Set, Map, etc.
+        Stream<Integer> collectionStream = collection.stream();
+
+        assertThat(collectionStream).contains(1, 2, 3);
+    }
+
+    @Test
+    void create_an_array_stream() {
+        Stream<Boolean> arrayStream = Stream.of(true, false);
+
+        assertThat(arrayStream).containsExactly(true, false);
+
+        String[] stringArray = new String[] {"a", "b", "c"};
+        Stream<String> fullArrayStream = Arrays.stream(stringArray);
+        Stream<String> partialArrayStream = Arrays.stream(stringArray, 1, 3);
+
+        assertThat(fullArrayStream).containsOnly("a", "b", "c");
+        assertThat(partialArrayStream).containsOnly("b", "c");
+    }
+
+    @Test
+    void be_created_with_a_builder() {
+        Stream<Integer> streamBuilder =     // Without <Integer> before builder() we get a stream of Object
+                Stream.<Integer>builder().add(1).add(2).add(3).build();
+
+        assertThat(streamBuilder).contains(1, 2, 3);
+    }
+
+    @Test
+    void be_created_with_iteration() {
+        Stream<Integer> oddNumbersUntilFour = Stream.iterate(0, n -> n + 2).limit(3);
+
+        assertThat(oddNumbersUntilFour).contains(0, 2, 4);
+    }
+
+    @Test
+    void create_primitive_streams() {
+        IntStream intStream = IntStream.range(1, 3);
+        LongStream longStream = LongStream.rangeClosed(1, 3);
+        Random random = new Random();
+        DoubleStream doubleStream = random.doubles(3);
+
+        assertThat(intStream).contains(1, 2);
+        assertThat(longStream).contains(1L, 2L, 3L);
+        assertThat(doubleStream).hasSize(3);
+    }
+}
